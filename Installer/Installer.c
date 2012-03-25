2e2b29d7093069a16c13543d5ff765e445510a4e
lilmnm616@gmail.com <lilmnm616@gmail.com@4fd8427c-d8a1-11de-9ba2-3f37f613d28e>
2012-03-25 13:11:44 +0000

    Fixed some pointers and added some error handling

--- Installer/Installer.c
+++ Installer/Installer.c
@@ -1,3 +1,27 @@
+/*    Name: DayPR, PSP Cheat Device
+*    Copyright (C) 2010 Joe "lilmnm-kamikaze-" Kiles
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or	
+*    (at your option) any later version.	
+*
+*    This program is distributed in the hope that it will be useful,	
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Notes:
+//Fixed the Yes_No Menu  decited to fix it up sense im upgrading it you know?
+//Right now this is  the normal Eboot style im gonna update it soon to be Better looking .-. wish me luck
+// 3/25/2012: Added some error handling maybe ill finish this logger and add that soon .-. fixed some
+// 			  pointers not being real pointers so i wonder how that has effected the app?
+//			  Added the license to ../License/License in the source tree
+
 //Includes
 #include <pspkernel.h>
 #include <pspiofilemgr.h>
@@ -13,7 +37,7 @@
 #include "Files/DAYPR.h" //files to be installed
 #include "Files/DAYPR371.h" // files to be installed
 
-PSP_MODULE_INFO("DayPR Installer", 0, 1, 1); //name of the module or PRX
+PSP_MODULE_INFO("DayPR Installer", 0, 1, 1); //name of the module and version (1.1) the 0 (zero) is the mode and its kernel mode
 
 // Some of my defines i use to help me  out  to code faster
 #define printf pspDebugScreenPrintf
@@ -29,7 +53,6 @@ unsigned int counter;
 unsigned int YesNo_Choice;
 unsigned char cheatButtonY=0;
 unsigned int Install_Choice=0;
-
 
 
 /* Exit callback */
@@ -63,56 +86,61 @@ int SetupCallbacks(void) {
 } 
 
 //some lib stuff i decited to make for this to help print text  faster and set its color
-void PrintTextColor(int color, const char *txt)
+void PrintTextColor(int color, const char* txt)
 	{
 		TextColor(color); //color of the text (shold always before the text to color it)
-		Text(txt); //the text to be written 
+		Text(*txt); //the text to be written 
 	}
 
 //Prints the text in a color and in a certin positon on the screen
-void PrintTextColorXY(int x, int y, int color, const char *txt)
+void PrintTextColorXY(int x, int y, int color, const char* txt)
 	{
 		SetXY(x, y); //locaton on the X axis and Y axis
 		TextColor(color); //color
-		Text(txt); // text
+		Text(*txt); // text
 	}
 
 // this clears a line so you can print somthing new in its place  and then puts a color to it
-void ClearLineTextColorY(int y, int color, const char *txt)
+void ClearLineTextColorY(int y, int color, const char* txt)
 	{
 		lineClear(y); // clears the line on the Y axis
 		TextColor(color); //color
-		Text(txt); //text
+		Text(*txt); //text
 	}
 
 //Prints  a title
 void print_title()
 {
-        PrintTextColorXY(0, 1, 0xFF0000FF, Install_Version);
-		SetXY(0, 3);
+        PrintTextColorXY(0, 1, 0xFF0000FF, Install_Version); //the title defined up above with a set position and color
+		SetXY(0, 3); //new position  to start from
         return;
 }
 
-void Install_Init() //installs files for the install
+void Install_Init() //Makes folders for the install
 { 	
 	pspDebugScreenClear();
 	print_title();
 	PrintTextColorXY(0, 3, 0xFF0000FF, "     Creating folders: ");
-	PrintTextColorXY(0, 4, 0xFF0000FF, "     Creating folders ms0:/seplugins/DayPR ");
+	PrintTextColorXY(0, 4, 0xFF0000FF, "     Creating folder ms0:/seplugins/DayPR/ ");
 	sceIoMkdir("ms0:/seplugins/DayPR", 0777); // creates the DayPR folder
-	sceKernelDelayThread(1000000); //microsecont delay for 1 secont
+	sceKernelDelayThread(1000000); //microsecond delay for 1 second
 	PrintTextColor(0xFF0000FF, "  DONE!!");
-	sceKernelDelayThread(500000); //microsecont delay for .5 secont
-	PrintTextColorXY(0, 5, 0xFF0000FF, "     Creating folders ms0:/seplugins/DayPR/dumps ");
+	sceKernelDelayThread(500000); //microsecond delay for .5 second
+	PrintTextColorXY(0, 5, 0xFF0000FF, "     Creating folder ms0:/seplugins/DayPR/dumps/ ");
 	sceIoMkdir("ms0:/seplugins/DayPR/dumps", 0777); //Creates the folder "dumps" in the DayPR folder for RAM dumps
-	sceKernelDelayThread(1000000); //microsecont delay for 1 secont
+	sceKernelDelayThread(1000000); //microsecond delay for 1 second
 	PrintTextColor(0xFF0000FF, "  DONE!!");
-	sceKernelDelayThread(500000); //microsecont delay for .5 secont
-	PrintTextColorXY(0, 6, 0xFF0000FF, "     Creating folders ms0:/seplugins/DayPR/search ");
+	sceKernelDelayThread(500000); //microsecond delay for .5 second
+	PrintTextColorXY(0, 6, 0xFF0000FF, "     Creating folder ms0:/seplugins/DayPR/search/ ");
 	sceIoMkdir("ms0:/seplugins/DayPR/search", 0777); //Creates the folder "search" in the DayPR folder for searches
-	sceKernelDelayThread(1000000);//microsecont delay for 1 secont
+	sceKernelDelayThread(1000000);//microsecond delay for 1 second
 	PrintTextColor(0xFF0000FF, "  DONE!!");
-	sceKernelDelayThread(1000000);//microsecont delay for 1 secont
+	sceKernelDelayThread(500000); //microsecond delay for .5 second
+	PrintTextColorXY(0, 7, 0xFF0000FF, "     Creating folder ms0:/seplugins/DayPR/ExtraCheats/ ");
+	sceIoMkdir("ms0:/seplugins/DayPR/ExtraCheats", 0777); //Creates the folder "ExtraCheats" in the DayPR folder for Your extra cheat DBs
+	sceKernelDelayThread(1000000);//microsecond delay for 1 second
+	PrintTextColor(0xFF0000FF, "  DONE!!");
+	sceKernelDelayThread(1000000);//microsecond delay for 1 second
 }
 
 /*
@@ -160,11 +188,11 @@ void ExitIn10()
 	}
 
 //Writes to the GAME.TXT as Enabled
-void gametxt_enabled()
+int gametxt_enabled()
 {
-	char *txtline = "ms0:/seplugins/%s.prx 1\n"; // text to be writen
+	char* txtline = "ms0:/seplugins/%s.prx 1\n"; // text to be writen
 	char txtdata[100];
-	sprintf(txtdata, txtline, "DayPR");
+	sprintf(txtdata, *txtline, "DayPR");
 	signed int fd0 = sceIoOpen("ms0:/seplugins/game.txt", PSP_O_WRONLY | PSP_O_CREAT, 0777);
 	sceIoLseek(fd0, 0, 2);
 	PrintTextColorXY(0, 9, 0xFF0000FF, "     Writing DayPR enabled to Game.txt...\n"); //Telling you what its doing what its doing
@@ -177,28 +205,28 @@ void gametxt_enabled()
 }
 
 //Writes the the GAME.TXT as Disabled
-void gametxt_disabled()
+int gametxt_disabled()
 {
-	char *txtline = "\nms0:/seplugins/%s.prx 0\n"; //Text to be writen
+	char* txtline = "\nms0:/seplugins/%s.prx 0\n"; //Text to be writen
 	char txtdata[100];
-	sprintf(txtdata, txtline, "DayPR");
+	sprintf(txtdata, *txtline, "DayPR");
 	signed int fd0 = sceIoOpen("ms0:/seplugins/game.txt", PSP_O_WRONLY | PSP_O_CREAT, 0777);
 	sceIoLseek(fd0, 0, 2);
 	PrintTextColorXY(0, 9, 0xFF0000FF, "     Writing DayPR disabled to Game.txt...\n"); //Telling you what its doing what its doing
 	sceIoWrite(fd0, txtdata, strlen(txtdata)); //Actually writes to the file
 	sceIoClose(fd0);
-	sceKernelDelayThread(200000); //waits 2 seconds
+	sceKernelDelayThread(200000); //waits .2 seconds
 	ClearLineTextColorY(9, 0xFF0000FF, "     Done!\n"); //tells you its done
 	sceKernelDelayThread(200000); //waits two seconds
 	return;
 }
 
-/* 
-
-This is to buggy for it to be left in so i took it out it started to piss me off so eh what ever fuck it I'll do it live!
-
 void Yes_No()
-	{
+{
+	while(1)
+	  {
+		sceCtrlReadBufferPositive(&pad, 1);
+		print_title(); // prints the title
 		PrintTextColorXY(0, 5, 0xFF0000FF, "  Would you like to write DayPR to GAME.txt?\n\n");
 		counter=0;
         while(counter < 3)
@@ -276,9 +304,9 @@ void Yes_No()
 						ExitIn10();
 					}
 			}
+	
 	}
-*/
-
+}
 
 //Installs thes normal DayPR 
 void Install_DayPR()
@@ -286,9 +314,18 @@ void Install_DayPR()
 		print_title(); // prints the title
 		PrintTextColorXY(0, 7, 0xFF0000FF, "     Installing DayPR.prx to ms0:/seplugins/..."); //Tells you what its doing
 		writeFile("ms0:/seplugins/DayPR.prx", DayPR, size_DayPR); //can be found in DAYPR.H
-		ClearLineTextColorY(7, 0xFF0000FF, "     Done!\n"); //Tells you its done
-		sceKernelDelayThread(1500000); //waits 1.5 seconds
-		//Yes_No();   eh dont worry about this
+		if(writeFile() > 0) //makes sure file has been writen
+		{
+			ClearLineTextColorY(7, 0xFF0000FF, "     Done!\n"); //Tells you its done
+			sceKernelDelayThread(1500000); //waits 1.5 seconds
+			Yes_No();
+		}
+		else
+		{
+			ClearLineTextColorY(7, 0xFF0000FF, "\n     ERROR! File has not been writen! Please try again!\n The installer will now exit...\n "); //Tells you its done
+			sceKernelDelayThread(1000000); //waits 1 second
+			ExitIn10();
+		}
 	}
 
 //Installs the 3.71 version of DayPR
@@ -297,12 +334,21 @@ void Install_DayPR371()
 		print_title(); // prints the title
 		PrintTextColorXY(0, 7, 0xFF0000FF, "     Installing DayPR.prx to ms0:/seplugins/..."); //Tells you what its doing
 		writeFile("ms0:/seplugins/DayPR.prx", DayPR371, size_DayPR371); //can be found at DAYPR371.h
-		ClearLineTextColorY(7, 0xFF0000FF, "     Done!\n"); //Tells you its done
-		sceKernelDelayThread(1500000); //waits 1.5 seconds
-		//Yes_No();	 menu is buggy to lazy to fix it right now =P
+		if(writeFile() > 0) //makes sure file has been writen
+		{
+			ClearLineTextColorY(7, 0xFF0000FF, "     Done!\n"); //Tells you its done
+			sceKernelDelayThread(1500000); //waits 1.5 seconds
+			Yes_No();
+		}
+		else
+		{
+			ClearLineTextColorY(7, 0xFF0000FF, "\n     ERROR! File has not been writen! Please try again!\n The installer will now exit...\n "); //Tells you its done
+			sceKernelDelayThread(1000000); //waits 1 second
+			ExitIn10();
+		}
 	}
-
-//Main menu
+	
+//Main menu	
 void Install()
 { 
   while(1)
@@ -310,8 +356,8 @@ void Install()
 		sceCtrlReadBufferPositive(&pad, 1);
 		print_title(); // prints the title
 		counter=0;
-        while(counter < 5)
-        {
+        while(counter < 3)
+        { 
           if(Install_Choice == counter)
           {
             //Highlight the selection
@@ -321,26 +367,24 @@ void Install()
           {
             //Don't highlight the selection
             TextColor(0xFFA9A9A9);
-          } 
+          }
           switch(counter)
           { //list of options
-            case 0: Text("     Install Enabled\n");break;
-			case 1: Text("     Install Disabled\n");break;
-			case 2: Text("     Install 3.71 version Enabled\n");break;
-			case 3: Text("     Install 3.71 version Disabled\n");break;
-			case 4: Text("     Exit\n");break;
+            case 0: Text("     Install Normal Version\n");break;
+			case 1: Text("     Install 3.71 version\n");break;
+			case 2: Text("     Exit\n");break;
 		  }
           counter++;
         }
 		
 		lineClear(30);
-        switch(Install_Choice)
+		
+        switch(
+		)
         { //Discriptions
-          case 0: PrintTextColor(0xFF0000FF, "  Install Normal DayPR Enabled In the Game.txt"); break;
-		  case 1: PrintTextColor(0xFF0000FF, "  Install Normal DayPR Disabled In the Game.txt"); break;
-          case 2: PrintTextColor(0xFF0000FF, "  Install 3.71 DayPR Enabled In the Game.txt"); break;
-		  case 3: PrintTextColor(0xFF0000FF, "  Install 3.71 DayPR Disabled In the Game.txt"); break;
-		  case 4: PrintTextColor(0xFF0000FF, "  Exit Installer"); break;
+          case 0: PrintTextColor(0xFF0000FF, "  Install Normal DayPR"); break;
+		  case 1: PrintTextColor(0xFF0000FF, "  Install 3.71 DayPR"); break;
+          case 2: PrintTextColor(0xFF0000FF, "  Exit Installer"); break;
 		}
 		
 		//control inputs and functions
@@ -352,17 +396,17 @@ void Install()
 					}
 				else if(Install_Choice == 0)
 					{
-						Install_Choice=4;
+						Install_Choice=2;
                     }
 				if(cheatButtonY < 12) cheatButtonY++; sceKernelDelayThread(150000-(10000*cheatButtonY));
 			}
         else if(pad.Buttons & PSP_CTRL_DOWN)
             {
-				if(Install_Choice < 4)
+				if(Install_Choice < 2)
 					{
 						Install_Choice++;
 					}
-				else if(Install_Choice == 4)
+				else if(Install_Choice == 2)
 					{
 						Install_Choice=0;
 					}
@@ -380,34 +424,16 @@ void Install()
 						Install_Init();
 						Install_DayPR();
 						sceKernelDelayThread(100000);
-						gametxt_enabled();
 					}
-				
+
 				else if(Install_Choice == 1)
-					{	
-						Install_Init();
-						Install_DayPR();
-						sceKernelDelayThread(100000);
-						gametxt_disabled();
-					}
-					
-				else if(Install_Choice == 2)
-					{	
-						Install_Init();
-						Install_DayPR371();
-						sceKernelDelayThread(100000);
-						gametxt_enabled();
-					}
-					
-				else if(Install_Choice == 3)
 					{
 						Install_Init();
 						Install_DayPR371();
 						sceKernelDelayThread(100000);
-						gametxt_disabled();
 					}
 
-				else if(Install_Choice == 4)
+				else if(Install_Choice == 2)
 					{
 						ExitIn10();
 					}	
@@ -420,7 +446,7 @@ int main(int argc, char *argv[])
 {
         SetupCallbacks();
         sceKernelDelayThread(1000000);
-        pspDebugScreenInit(); 
+        pspDebugScreenInit();
 		SetXY(0, 0);
         Install();
         ExitIn10();
